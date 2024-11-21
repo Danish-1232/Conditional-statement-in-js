917bc4e9af9a1c590d220c5ed1ea85c1e2ba52e6
Danish-Farooqui <danishfarooqui303@gmail.com>
2024-11-21 10:08:37 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,131 @@
+// No 1
+
+// var age = 9;
+// if (age >= 10){
+//     console.log("Greater Than 10");
+// } else {
+//     console.log("Not Greater Than 10");
+// }
+
+// No 2
+
+// var age = 30;
+// if (age >=18){
+//     console.log("Eligible to vote");
+// } else {
+//     console.log("Not eligible to vote");
+// }
+
+// No 3
+
+
+var num = 96;
+if (num <=95){
+    console.log("Right Number");
+} else {
+    console.log("Wrong Number");
+}
+
+// No 4
+
+// let mark = 85;
+// if (mark>=90) {
+//     document.write("Grade A")
+
+// }
+//  else if (mark>= 80) {
+//     document.write("Grade B")
+// }
+// else {
+//     document.write("Grade C")
+// }
+
+
+var table = 14;
+if (table<20){
+    console.log("The Table Is Right");
+}else {
+    console.log("The Table IS Wrong");
+}
+
+
+//  NO 5
+
+// let Temperature = 18;
+// if (Temperature>30){
+//     console.log("Its Hot Outside");
+// }else{
+//     console.log("The Weather Is Cool");
+// }
+
+//NO 6
+
+let password = "danish123";
+if(password.length >=8){
+    console.log("Password Is Strong");
+}else {
+    console.log("Password Is Weak");
+}
+
+
+// NO 7
+
+let score = 49;
+if (score >= 50){
+    console.log("Pass");
+}else{
+console.log("Fail");
+}
+
+// NO 8
+
+
+let scor = 95;
+if(scor>=90){
+
+    console.log("Grade A");
+} else if (scor>=80){
+
+    console.log(" Grade B");
+} else{
+    console.log("Grade C");
+}
+
+
+// NO 9
+
+var Temperature = 4;
+if(Temperature>=10){
+    console.log("it is Hot Outside");
+} else if(Temperature>=20){
+    console.log("It Is Warm Outside ");
+}else if (Temperature>=5){
+    console.log("It IS Cool");
+}else{
+    console.log("It Is Cold");
+}
+
+
+// NO 10
+
+let amount = 45;
+if(amount>= 100){
+   console.log("You Get a 20 % Discount");
+} else if (amount>=50){
+    console.log("You Get a 10 % Discount");
+}else {
+    console.log("No Discount");
+}
+
+// NO 11
+
+let age = 70;
+if(age <= 13){
+    console.log("You Are a Child");
+}else if (age <= 19){
+    console.log("You Are a Teenager");
+}else if (age <= 64){
+    console.log("You Are an Adult");
+}else{
+    console.log("You Are a Senior");
+}
